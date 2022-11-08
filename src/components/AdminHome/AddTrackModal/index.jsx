33a5d381aff165555d86c43dd4dccde3d342c777
almeidaxz <almeidaxz@live.com>
2022-11-08 10:08:27 -0300

    implementado com o backend e já retornando erros e cadastrando no BD

--- src/components/AdminHome/AddTrackModal/index.jsx
+++ src/components/AdminHome/AddTrackModal/index.jsx
@@ -1,17 +1,24 @@
+import { useState } from 'react';
 import '../../../styles/utils.css';
 import ConfirmModal from "../ConfirmModal";
 import { TrackModal } from "./styled";
 
 export default function AddTrackModal({ confirmModal, setConfirmModal, setOpenModal }) {
+    const [form, setForm] = useState({ name: '', error: '', status: 'Incompleta' });
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    }
 
     return (
         <div>
-            {confirmModal && <ConfirmModal setConfirmModal={setConfirmModal} setOpenModal={setOpenModal} />}
+            {confirmModal && <ConfirmModal form={form} setForm={setForm} setConfirmModal={setConfirmModal} setOpenModal={setOpenModal} />}
             <div className="modal-bg">
                 <TrackModal className="column gap-24">
                     <div className="column">
                         <label htmlFor="">Nome da trilha</label>
-                        <input type="text" placeholder="Digite o nome da trilha" />
+                        <input onChange={handleChange} name='name' value={form.name} type="text" placeholder="Digite o nome da trilha" />
+                        {form.error && <span>{form.error}</span>}
                     </div>
                     <div className="column">
                         <button onClick={(e) => setConfirmModal(!confirmModal)}>Acicionar Trilha</button>
--- src/components/AdminHome/ConfirmModal/index.jsx
+++ src/components/AdminHome/ConfirmModal/index.jsx
@@ -1,10 +1,21 @@
 import { ConfirmCard } from './styled';
+import api from '../../../services/api';
 import '../../../styles/utils.css';
 
-export default function ConfirmModal({ setConfirmModal, setOpenModal }) {
-    const handleClearStates = () => {
-        setConfirmModal(false);
-        setOpenModal(false);
+export default function ConfirmModal({ setConfirmModal, setOpenModal, form, setForm }) {
+    const handleClearStates = async () => {
+        try {
+            const { error: _, ...formData } = form;
+
+            const response = await api.post('/admin/add_track', formData);
+
+            setConfirmModal(false);
+            setOpenModal(false);
+            setForm({ name: '', error: '', status: 'Incompleta' });
+        } catch (error) {
+            setConfirmModal(false);
+            setForm({ ...form, error: error.response.data.message });
+        }
     }
 
     return (
