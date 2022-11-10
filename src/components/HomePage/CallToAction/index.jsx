6ce378587b7c6b828436728164cd64fa6444f860
almeidaxz <almeidaxz@live.com>
2022-11-10 00:16:05 -0300

    fix nos botões secundários

--- src/components/HomePage/CallToAction/index.jsx
+++ src/components/HomePage/CallToAction/index.jsx
@@ -8,7 +8,7 @@ export default function CallToAction() {
             <Title>Boas vindas à <Brand>Orange Evolution</Brand></Title>
             <Subtitle>Estamos de casa nova!</Subtitle>
             <Text>Explore conhecimentos que estão transformando indústrias, negócios e vidas através de trilhas gratuitas em <strong>Desenvolvimento, UX/UI Design e QA!</strong></Text>
-            <ShowTracksButton>
+            <ShowTracksButton className='secondary-button'>
                 <HashLink to="/home#tracks-list">
                     Conheça as trilhas
                 </HashLink>
