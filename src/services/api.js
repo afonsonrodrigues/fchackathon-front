317e4824b435e1a49b55aef3ad0b61d7fd4594a8
almeidaxz <almeidaxz@live.com>
2022-11-14 11:12:56 -0300

    deploy responsividade na /login

--- src/services/api.js
+++ src/services/api.js
@@ -3,9 +3,5 @@ import axios from "axios";
 export default axios.create({
     // baseURL: 'http://localhost:3000',
     baseURL: 'https://squad27-server.up.railway.app',
-    timeout: 10000,
-    headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-    }
+    timeout: 10000
 });
