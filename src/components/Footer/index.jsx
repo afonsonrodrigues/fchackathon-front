3181bb92074730bf5137297f15d4e5829f64beaf
Afonso Rodrigues <afonso.rodrigues@outlook.com>
2022-11-15 19:06:30 -0300

    feat: Adiciona nome ao NavBar

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,73 +1,102 @@
-import { NavLink } from 'react-router-dom';
-import OrangeBrand from '../../assets/orange-brand.svg';
-import LinkAnimationContainer from '../LinkAnimationContainer';
-import '../../styles/utils.css';
-import { BrandContainer, CustomFooter, LinksWrapper, RightContainer, RightContainerTitle } from './styled';
+import { NavLink } from "react-router-dom";
+import OrangeBrand from "../../assets/orange-brand.svg";
+import LinkAnimationContainer from "../LinkAnimationContainer";
+import "../../styles/utils.css";
+import {
+    BrandContainer,
+    CustomFooter,
+    LinksWrapper,
+    RightContainer,
+    RightContainerTitle,
+} from "./styled";
 
 export default function Footer() {
     return (
-        <CustomFooter className='flex flex-col pb-8 lg:pb-0 items-center w-full sm:items-start sm:px-10 sm:h-[309px] lg:h-[250px] lg:items-center lg:justify-center'>
-            <div className='lg:flex lg:justify-between lg:w-[1024px]'>
-                <BrandContainer className='pt-14 mb-8 lg:pt-0 lg:flex lg:flex-col lg:justify-between'>
+        <CustomFooter className="flex flex-col pb-8 lg:pb-0 items-center w-full sm:items-start sm:px-10 sm:h-[309px] lg:h-[250px] lg:items-center lg:justify-center">
+            <div className="lg:flex lg:justify-between lg:w-[1024px]">
+                <BrandContainer className="pt-14 mb-8 lg:pt-0 lg:flex lg:flex-col lg:justify-between">
                     <img src={OrangeBrand} alt="Orange Evolution brand" />
                 </BrandContainer>
-                <RightContainer className='flex flex-col'>
-                    <RightContainerTitle className='mb-12 pt-2'>
+                <RightContainer className="flex flex-col">
+                    <RightContainerTitle className="mb-12 pt-2">
                         Conheça o ecossistema tech Orange Juice
                     </RightContainerTitle>
-                    <LinksWrapper className='flex flex-col px-4 sm:h-24 flex-wrap gap-x-8'>
+                    <LinksWrapper className="flex flex-col px-4 sm:h-24 flex-wrap gap-x-8">
                         <LinkAnimationContainer>
-                            <NavLink
-                                name='1'
+                            <a
+                                name="1"
+                                href="https://digital.fcamara.com.br/orangejuice"
+                                target={"_blank"}
                             >
                                 Orange Juice
-                            </NavLink>
+                            </a>
                         </LinkAnimationContainer>
                         <LinkAnimationContainer>
-                            <NavLink
-                                name='2'>
+                            <a
+                                name="2"
+                                href="https://digital.fcamara.com.br/orangejuice#rd-section-l5h5qpid"
+                                target={"_blank"}
+                            >
                                 Orange Hub
-                            </NavLink>
+                            </a>
                         </LinkAnimationContainer>
                         <LinkAnimationContainer>
-                            <NavLink
-                                name='3'>
+                            <a
+                                name="3"
+                                href="https://medium.com/orangejuicefc"
+                                target={"_blank"}
+                            >
                                 Orange Blog
-                            </NavLink>
+                            </a>
                         </LinkAnimationContainer>
                         <LinkAnimationContainer>
-                            <NavLink
-                                name='5'>
+                            <a
+                                name="5"
+                                href="https://www.youtube.com/orangejuicefc"
+                                target={"_blank"}
+                            >
                                 Orange Talks
-                            </NavLink>
+                            </a>
                         </LinkAnimationContainer>
                         <LinkAnimationContainer>
-                            <NavLink
-                                name='6'>
+                            <a
+                                name="6"
+                                href="https://open.spotify.com/show/5zy6DpF8V3N8rBbWO7OE79?si=45d0140798064511"
+                                target={"_blank"}
+                            >
                                 Orange Cast
-                            </NavLink>
+                            </a>
                         </LinkAnimationContainer>
                         <LinkAnimationContainer>
-                            <NavLink
-                                name='7'>
+                            <a
+                                name="7"
+                                href="https://digital.fcamara.com.br/programadeformacao"
+                                target={"_blank"}
+                            >
                                 Programa de formação
-                            </NavLink>
+                            </a>
                         </LinkAnimationContainer>
                         <LinkAnimationContainer>
-                            <NavLink
-                                name='8'>
+                            <a
+                                name="8"
+                                href="https://digital.fcamara.com.br/orangejuice#rd-section-joq3m2lv"
+                                target={"_blank"}
+                            >
                                 FAQ
-                            </NavLink>
+                            </a>
                         </LinkAnimationContainer>
                         <LinkAnimationContainer>
-                            <NavLink
-                                name='9'>
+                            <a
+                                name="9"
+                                href="https://www.fcamara.com.br/contato"
+                                target={"_blank"}
+                            >
                                 Contato
-                            </NavLink>
+                            </a>
                         </LinkAnimationContainer>
                     </LinksWrapper>
                 </RightContainer>
             </div>
-        </CustomFooter >
-    )
+        </CustomFooter>
+    );
 }
